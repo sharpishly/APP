30775533bf635d1d13d69d21a52f1e47193cf33c
Steve Austin <steve@austin.com>
2025-08-09 15:38:29 +0100

    Added shut down Nginx command to git action deployment file. After system reboot Nginx is re-activated. This is a temporary measure. Created development environment Closed #1

--- sharpishly.com/website/public/index.php
+++ sharpishly.com/website/public/index.php
@@ -44,7 +44,7 @@
         <section class="grid md:grid-cols-2 gap-8">
             <!-- Left Column -->
             <div class="bg-white rounded-lg shadow-md p-6">
-                <h3 class="text-xl font-semibold text-gray-800 mb-2">Column One</h3>
+                <h3 class="text-xl font-semibold text-gray-800 mb-2">Software Development</h3>
                 <p class="text-gray-600 leading-relaxed">
                     Sharpihly goal is to make AI accessible to everyone.
                 </p>
