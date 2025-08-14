d52dd63e74129b692ac447168a5bc601280b71cf
Steve Austin <steve@austin.com>
2025-08-14 14:42:41 +0100

    #32 Removed duplicate docker-compose.yml in an attempt to remove any confusion from future commit. Updated docker-compose.yml with a command directive for mysql

--- dev.sharpishly.com/website/public/index.php
+++ dev.sharpishly.com/website/public/index.php
@@ -1,20 +1,10 @@
 <?php
 
-echo $dir = dirname(dirname(__FILE__));
+$dir = dirname(dirname(__FILE__));
 
 require_once $dir . '/env.php';
 
-// Output the database credentials
-echo "Database Credentials:\n";
-echo "DB_HOST: " . DB_HOST . "\n";
-echo "DB_USER: " . DB_USER . "\n";
-echo "DB_PASSWORD: " . DB_PASSWORD . "\n";
-echo "DB_NAME: " . DB_NAME . "\n";
-
-// Optional: Output $_SERVER['HTTP_HOST'] to verify domain
-echo "\nServer Host: " . $_SERVER['HTTP_HOST'] . "\n";
-
-echo $vendor = $dir . "/vendor/autoload.php";
+$vendor = $dir . "/vendor/autoload.php";
 
 require $vendor;
 
@@ -26,16 +16,7 @@ if (session_status() == PHP_SESSION_NONE) { // Prevent multiple calls
     session_start();
 }
 
-// Add these debugging logs to your index.php
-error_log("INDEX.PHP: Script start.");
-error_log("INDEX.PHP: Session ID: " . session_id());
-error_log("INDEX.PHP: Current \$_SESSION content (before App init): " . print_r($_SESSION, true));
-
 $conf['dir'] = $dir;
 
-print_r($conf);
-
 $app = new App($conf);
-
-print_r($app);
 
